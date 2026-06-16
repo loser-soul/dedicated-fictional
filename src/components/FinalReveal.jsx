6862b060512d13129904861d8113cac87e7ae748
loser-soul <fake.hai7799@gmail.com>
2026-06-16 17:07:13 +0530

    Update FinalReveal.jsx

--- src/components/FinalReveal.jsx
+++ src/components/FinalReveal.jsx
@@ -1,21 +1,50 @@
 import './FinalReveal.css'
+import { useEffect, useRef } from 'react'
 
 function FinalReveal() {
+
+  const audioRef = useRef(null)
+
+  useEffect(() => {
+
+    if(audioRef.current){
+
+      audioRef.current.volume = 1
+
+      audioRef.current.play()
+
+    }
+
+  }, [])
 
   return (
 
     <div className="reveal-container">
 
+      {/* AUDIO */}
+
+      <audio
+        ref={audioRef}
+        src="/prank.mp3"
+      />
+
+      {/* IMAGE */}
+
       <img
-        src="/dedicated-fictional/prank.gif"
+        src="/prank.gif"
         alt="Prank"
         className="reveal-image"
       />
 
+      {/* OVERLAY */}
+
       <div className="reveal-overlay">
-        <h2>
-        Apna kaam se matlab rakha kar bsdk! 
-        </h2>
+
+        
+
+        <h3>
+          Wapis chala jaa BSDK!
+        </h3>
 
       </div>
 
